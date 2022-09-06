72eca8d274b73624f2b8e572409386cfd05b5378
Damien <damienroot@icloud.com>
2022-09-06 16:05:29 +0100

    create footer.js

---
+++ unity/src/data/footer.js
@@ -0,0 +1,63 @@
+export const languageLinks = [
+  "English",
+  "Español",
+  "Français",
+  "中文",
+  "日本語",
+  "한국어",
+  "Deutsch",
+  "Русский",
+  "Português",
+];
+
+export const mainLinks = {
+  purchase: [
+    "Products",
+    "Unity Ads",
+    "Subscription",
+    "Asset Store",
+    "Resellers",
+  ],
+  education: [
+    "Students",
+    "Educators",
+    "Certification",
+    "Learn",
+    "Center of Excellence",
+  ],
+  download: ["Get Unity", "Download Archive", "Beta Program"],
+  unityLabs: ["Labs", "Publications"],
+  resources: [
+    "Learn platform",
+    "Community",
+    "Documentation",
+    "Unity QA",
+    "FAQ",
+    "Services Status",
+    "Case Studies",
+    "Made with Unity",
+  ],
+  unity: [
+    "Our Company",
+    "Brand",
+    "Newsletter",
+    "Blog",
+    "Events",
+    "Careers",
+    "Help",
+    "Press",
+    "Partners",
+    "Investors",
+    "Affiliates",
+    "Security",
+    "Social Impact",
+  ],
+};
+
+export const copyrightLinks = [
+  "Legal",
+  "Privacy Policy",
+  "Cookies",
+  "Do Not Sell My Personal Information",
+  "Cookies Settings",
+];
