5befd2523aea5e501641447a9c5ccbcf78245f16
Damien <damienroot@icloud.com>
2022-09-07 15:26:36 +0100

    create sponsors.js
    and import images

---
+++ unity/src/data/sponsors.js
@@ -0,0 +1,39 @@
+import logoIOS from "../assets/images/ios.png";
+import logoAdroid from "../assets/images/android.png";
+import logoWindows from "../assets/images/windows.png";
+import logoUWP from "../assets/images/universal-windows-platform.png";
+import logoLinux from "../assets/images/linux.png";
+import logoWebGL from "../assets/images/webgl.png";
+import logoPS4 from "../assets/images/ps4.png";
+import logoPS5 from "../assets/images/ps5.jpg";
+import logoXboxOne from "../assets/images/xbox-one.png";
+import logoXboxSeriesXS from "../assets/images/xbox-series-x-s.jpg";
+import logoOculusRift from "../assets/images/oculus-rift.png";
+import logoAndroidTV from "../assets/images/androidtv.png";
+import logoTVOS from "../assets/images/tvos.png";
+import logoNintendoSwitch from "../assets/images/nintendo-switch.png";
+import logoARCore from "../assets/images/arcore.png";
+import logoStadia from "../assets/images/stadia.png";
+import logoMicrosoftHoloLens from "../assets/images/microsoft-hololens.png";
+import logoMagicLeap from "../assets/images/magic-leap.png";
+
+export const logos = [
+  logoIOS,
+  logoAdroid,
+  logoWindows,
+  logoUWP,
+  logoLinux,
+  logoWebGL,
+  logoPS4,
+  logoPS5,
+  logoXboxOne,
+  logoXboxSeriesXS,
+  logoOculusRift,
+  logoAndroidTV,
+  logoTVOS,
+  logoNintendoSwitch,
+  logoARCore,
+  logoStadia,
+  logoMicrosoftHoloLens,
+  logoMagicLeap,
+];
